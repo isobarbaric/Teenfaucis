2c045e29816e2e847a23145d34146d90205d7545
isobarbaric <shahkrish2016@gmail.com>
2022-01-09 03:46:17 -0500

    tweaked peripherals

--- finder.py
+++ finder.py
@@ -5,13 +5,12 @@ import os
 def find_articles():
     base_path = "news_channels/" # used as prefix for file path when creating files in the news_channels folder 
     structure = os.listdir('news_channels/') # returns a list containing all of the websites that were visited by crawler() to allow iteration
-    identifier = ['post', 'article', 'entry', 'news', 'heading', 'header'] # words that appear often in the 
+    identifier = ['post', 'article', 'entry', 'news', 'heading', 'header'] # words that appear often in html attribute descriptions for posts 
     # below list contains keywords that the program searches for in the heading section of all possible articles to determine whether they are about COVID-19 or not 
-    covid_keywords = ['COVID', 'COVID-19', 'covid', 'pandemic', 'virus', 'Omicron', 'omicron', 'Delta', 'delta', 'variant', 'outbreak', 'wave', 'symptoms', 'testing', 'rapid test', 'pcr', 'PCR']
+    covid_keywords = ['COVID', 'COVID-19', 'covid', 'pandemic', 'Pandemic', 'virus', 'Omicron', 'omicron', 'Delta', 'delta', 'variant', 'outbreak', 'mask', 'N95', 'KN95', 'wave', 'symptoms', 'testing', 'rapid test', 'pcr', 'PCR', 'social distancing', 'Social distancing', 'Social Distancing', 'epidemic', 'Epidemic', 'fatality rate', 'Fatality rate', 'Fatality Rate', 'flattening the curve', 'Flattening the Curve']
     # below list contains html elements where specifications are provided that can be used to determine whether certain text represents a news article's header or not 
     where_to_look = ['div', 'section', 'span']
-    # below list will contain the final result, namely a list of news article names and links to those news articles
-    overall = []
+    overall = [] # list will contain the final result, namely a list of news article names and links to those news articles
     for folder in structure:
         current_path = base_path + folder # specifies file path to the current file under consideration 
         with open(current_path, 'r') as current_soup: # opening the file and initializing a soup object
@@ -36,13 +35,15 @@ def find_articles():
                 mod_title = article_title[1].text.replace('\\n', '').replace('\t', '').replace('\\r', '').replace('\\', '')
                 mod_title = ' '.join(mod_title.split())
                 # mod_title is the title of the article (is cleaned up a bit from its rough and unfiltered form when it is obtained)
+                if 'css' in mod_title:
+                    continue
                 for potential in identifier: # this for loop determines whether this particular article title is actually refering the news headline or not 
                     if potential in article_title[0]['class']:
                         overlap = True
                 for covid_word in covid_keywords: # this for loop checks to see if a particular article contains a reference to COVID or not 
                     if covid_word in mod_title:
                         covid_related = True
-                if overlap and covid_related: # if an article passes both tests, then it is a valid article and it is passed to the final data structure 
+                if covid_related: # if an article passes both tests, then it is a valid article and it is passed to the final data structure 
                     overall.append([mod_title, article_title[1]['href']]) 
                     covid_related = False
                     overlap = False
@@ -51,4 +52,4 @@ def find_articles():
     for elem in overall: 
         if elem not in resultant:
             resultant.append(elem)
-    return resultant 
+    return resultant
